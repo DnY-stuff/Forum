4e36841f70725787d5ba872bfa27d86e2b020b22
Dongkyu Shin <dkshin67@gmail.com>
2011-03-23 22:35:14 +0900

    IThreadRepository Parameter 수정

--- Source/DnY.Forum.Model/Model/Thread/IThreadRepository.cs
+++ Source/DnY.Forum.Model/Model/Thread/IThreadRepository.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace DnY.Forum.Domain.Model
 {
     public interface IThreadRepository
     {
-        Thread Find(Guid id);
+        Thread Find(Guid threadId);
         void Store(Thread thread);
     }
 }
